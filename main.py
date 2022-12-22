16ecd4c128ab042bf76638206e9362c54179b999
kk-r-on <kazu.2003.onoda@gmail.com>
2022-12-22 15:19:56 +0900

    出力する画像の取得

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import pathlib
+from out_img import out_img
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
 
@@ -10,7 +11,9 @@ def index():
 
 @app.route('/outout')
 def upload():
-    return render_template("output.html")
+    ps = 'aaa'
+    img = out_img(ps)
+    return render_template("output.html",img)
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
---
+++ out_img.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+
+def out_img(self,ps):
+    img = cv2.imread(ps)
+    return img
