c6b306a5bd978a0500e68af3ad26a9056f86b3a5
kk-r-on <kazu.2003.onoda@gmail.com>
2023-01-12 14:42:49 +0900

    エラー修正

--- Sort.py
+++ Sort.py
@@ -3,72 +3,73 @@ import pathlib
 import numpy as np
 import json
 
-#仮のRGB
-x = 125
-y = 125
-z = 125
+def Sort(r, g, b):
 
-start = 1000000000
-        
-fg = 0
-fb = 0
-fr = 0
+    x = r
+    y = g
+    z = b
 
-sumg = 0
-sumb = 0
-sumr = 0
+    start = 1000000000
+            
+    fg = 0
+    fb = 0
+    fr = 0
 
-count = 0
+    sumg = 0
+    sumb = 0
+    sumr = 0
 
-input_dir = "images"
-input_list = list(pathlib.Path(input_dir).glob('**/*.jpg'))
+    count = 0
 
-
-for i in range(len(input_list)):
-    img_file_name = str(input_list[i])
-    img_np = np.fromfile(img_file_name, dtype=np.uint8)
-    img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-    g_hight, g_width, g_channel = img.shape
+    input_dir = "images"
+    input_list = list(pathlib.Path(input_dir).glob('**/*.jpg'))
 
 
-    for x in range(g_width):
-        for y in range(g_hight):
-            g, b, r = img[y, x]
-                    #print(vv_img[y, x, :])
-                    
-            sumg = abs(g-x) + sumg
-            sumb = abs(b-y) + sumb
-            sumr = abs(r-z) + sumr
-            sumall = sumg + sumb + sumr
-        
-    print(sumg)
-    print(sumb)
-    print(sumr)
-    print(sumall)
-    print("----------------")
+    for i in range(len(input_list)):
+        img_file_name = str(input_list[i])
+        img_np = np.fromfile(img_file_name, dtype=np.uint8)
+        img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+        g_hight, g_width, g_channel = img.shape
 
-    if(start>sumall):
-        start = sumall
-        fg = sumg
-        fb = sumb
-        fr = sumr
-        count = i
 
-data = {
-    "path": str(input_list[count]),
-    'g': str(fg),
-    'b': str(fb),
-    'r': str(fr)
-}
+        for x in range(g_width):
+            for y in range(g_hight):
+                g, b, r = img[y, x]
+                        #print(vv_img[y, x, :])
+                        
+                sumg = abs(g-x) + sumg
+                sumb = abs(b-y) + sumb
+                sumr = abs(r-z) + sumr
+                sumall = sumg + sumb + sumr
+            
+        print(sumg)
+        print(sumb)
+        print(sumr)
+        print(sumall)
+        print("----------------")
 
-path2 = "./Data/data.json"
-json_file2 = open(path2, mode = "w")
-json.dump(data, json_file2, ensure_ascii=False)
-json_file2.close()
+        if(start>sumall):
+            start = sumall
+            fg = sumg
+            fb = sumb
+            fr = sumr
+            count = i
 
-with open('./Data/data.json') as f:
-    jsn = json.load(f)
+    data = {
+        "path": str(input_list[count]),
+        'g': str(fg),
+        'b': str(fb),
+        'r': str(fr)
+    }
 
-print(jsn["path"])
-print(type(jsn["path"]))
-#cv2.imwrite('god2.jpeg', god_img)
+    path2 = "./Data/data.json"
+    json_file2 = open(path2, mode = "w")
+    json.dump(data, json_file2, ensure_ascii=False)
+    json_file2.close()
+
+    with open('./Data/data.json') as f:
+        jsn = json.load(f)
+
+    print(jsn["path"])
+    print(type(jsn["path"]))
+    #cv2.imwrite('god2.jpeg', god_img)
