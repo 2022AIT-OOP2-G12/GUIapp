b749e7fe9e5c5ad094210ae07fcbba616e150070
kk-r-on <kazu.2003.onoda@gmail.com>
2023-01-12 13:46:27 +0900

    main.pyとindex.htmlの調整
    コメントで変更点、補足を記入

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request,render_template
 import pathlib
 from out_img import out_img
 app = Flask(__name__)
@@ -9,11 +9,15 @@ app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列
 def index():
     return render_template("index.html")
 
-@app.route('/output')
-def upload():
+@app.route('/output', methods=["POST"])
+def output():
+    r = request.form.get("colorvalue_r")
+    g = request.form.get("colorvalue_g")
+    b = request.form.get("colorvalue_b")
+    #取得したRGBを引数として一番近い画像のパスを取得
     ps = 'aaa'
     img = out_img(ps)
-    return render_template("output.html",img)
+    return render_template("output.html",r,g,b,img)
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
